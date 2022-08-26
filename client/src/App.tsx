96cfd5ccf4db7ceb198f0dd143cc794b3b2a339e
unknown <nicomugas@gmail.com>
2022-08-26 10:33:53 -0300

    ModificandoBoilerPlateyAgregandoComponents

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { HashRouter as Router, Route, Link, Routes } from 'react-router-dom'
-import Landing from './components/Landing/Landing';
-import Home from './components/Home/Home';
+import Landing from './pages/Landing/Landing';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
