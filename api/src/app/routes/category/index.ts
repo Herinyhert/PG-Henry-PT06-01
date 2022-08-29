500948f639fe6664ea2230103e0e9c4b9446ec87
alejandrobalonga <alejandrobalonga@gmail.com>
2022-08-28 21:09:41 -0300

    agregando cositas

--- api/src/app/routes/category/index.ts
+++ api/src/app/routes/category/index.ts
@@ -1,17 +1,27 @@
-import { Router } from "express"
+import { Router } from "express";
 import prisma from "../../../db";
 
-
-const categoryRoutes = Router()
+const categoryRoutes = Router();
 
 categoryRoutes.post("/", async (req, res) => {
-    const { name } = req.body
+  try {
+    const { name } = req.body;
+    if (typeof name !== "string") {
+      res.status(400).json({ message: `the 'name' must be a string` });
+      return;
+    }
+    
     const newCategory = await prisma.category.create({
-        data: {
-            name: name,
-        },
-    })
-    res.status(200).send(newCategory)
+      data: {
+        name: name,
+      },
+    });
+    res.status(200).json(newCategory);
+  } catch (error) {
+    console.log("post category fail", error);
+    res.status(400).json({ message: `post category fail ${error}` });
+    return;
+  }
 });
 
-export default categoryRoutes
+export default categoryRoutes;
