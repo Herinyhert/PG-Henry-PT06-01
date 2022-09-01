b6b309000d0c7c1387f78ce29c14a76c2323c29c
Irupe <lilianirupemeza@gmail.com>
2022-08-31 22:00:04 -0300

    CRUD de rutas Rol

--- api/src/app/routes/index.ts
+++ api/src/app/routes/index.ts
@@ -1,12 +1,14 @@
 import Router from "express";
 import productRoutes from "./product";
 import categoryRoutes from "./category";
-import imageRoutes from "./image"
+import imageRoutes from "./image";
+import backofficeRoutes from "./Rol"
 
 const router = Router();
 
 router.use("/product", productRoutes);
 router.use("/category", categoryRoutes);
-router.use("/image", imageRoutes)
+router.use("/image", imageRoutes);
+router.use("/backoffice", backofficeRoutes);
 
 export default router;
