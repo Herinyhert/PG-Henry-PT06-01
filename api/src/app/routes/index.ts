19f55bc5bef22d52cb80a292ed51b4daf57fce07
unknown <nicomugas@gmail.com>
2022-09-03 20:18:21 -0300

    MercadoPago y Post pago

--- api/src/app/routes/index.ts
+++ api/src/app/routes/index.ts
@@ -2,14 +2,12 @@ import Router from 'express';
 import productRoutes from './product';
 import categoryRoutes from './category';
 import imageRoutes from './image';
-<<<<<<< Updated upstream
-=======
-// import backofficeRoutesRol from './Rol';
->>>>>>> Stashed changes
 import backofficeRoutesOrder from './Order/Order';
 import userRoutes from './user';
 import authRouter from './auth';
 import pruebaRoutes from './prueba/prueba';
+import mercadoPagoRoutes from './mercadopago';
+
 
 const router = Router();
 
@@ -17,12 +15,9 @@ router.use('/product', productRoutes);
 router.use('/category', categoryRoutes);
 router.use('/image', imageRoutes);
 router.use('/user', userRoutes);
-<<<<<<< Updated upstream
-=======
-// router.use('/backoffice/rol', backofficeRoutesRol);
->>>>>>> Stashed changes
 router.use('/backoffice/order', backofficeRoutesOrder);
 router.use('/auth', authRouter)
 router.use('/prueba', pruebaRoutes)
+router.use('/mercadopago', mercadoPagoRoutes)
 
 export default router;
