a181081ebf5d528cdea7eec87d41a8c64215e6be
maalroba22 <maalroba22@gmail.com>
2022-09-07 15:11:29 -0500

    paswod compared

--- api/src/app/routes/auth/index.ts
+++ api/src/app/routes/auth/index.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
-import { Express } from "express";
-import prisma from "../../../db";
-import jwt from 'jsonwebtoken'
-import config from '../../config/config'
-const bcrypt = require("bcrypt");
+import { Router } from 'express';
+import { Express } from 'express';
+import prisma from '../../../db';
+import jwt from 'jsonwebtoken';
+import config from '../../config/config';
+const bcrypt = require('bcrypt');
 
 const authRouter = Router();
 
@@ -16,22 +16,24 @@ export interface userInt {
   role: string;
 }
 
-function createToken (user: userInt){
- return jwt.sign({id: user.id, email: user.email, role: user.role}, config.jwtsecret)
+function createToken(user: userInt) {
+  return jwt.sign(
+    { id: user.id, email: user.email, role: user.role },
+    config.jwtsecret
+  );
 }
 
-authRouter.post("/signup", async (req, res) => {
-
+authRouter.post('/signup', async (req, res) => {
   const { email, password, name, surname } = req.body;
   //hasheamos el password
   let salt = await bcrypt.genSalt(10);
   let passwordHash = await bcrypt.hash(password, salt);
-  console.log(email)
+  console.log(email);
   if (!name || !surname) {
-    res.status(400).send("name y surname");
+    res.status(400).send('name y surname');
   }
   if (!email || !password) {
-    res.status(400).send("ingrese usuario y contraseña");
+    res.status(400).send('ingrese usuario y contraseña');
   }
   const trueUser = await prisma.user.findUnique({
     where: {
@@ -49,32 +51,37 @@ authRouter.post("/signup", async (req, res) => {
     });
     res.status(200).send(newUser);
   } else {
-    res.status(400).send("el usuario ya existe");
+    res.status(400).send('el usuario ya existe');
   }
 });
 
-authRouter.post("/signin", async (req, res) => {
+authRouter.post('/signin', async (req, res) => {
   const { email, password } = req.body;
-  // console.log(email, password)
   if (!email || !password) {
-    res.status(400).send("ingrese usuario y contraseña");
+    res.status(400).send('ingrese usuario y contraseña');
   }
   const user = await prisma.user.findUnique({
     where: {
       email: email,
     },
   });
-  if(!user){
-    res.status(400).send("el usuario no existe");
-  }else{
-    if(user.password === password){
+  if (!user) {
+    res.status(400).send('el usuario no existe');
+  } else {
+    // revisar el pasword------/
+    const passworCorrecto = await bcrypt.compare(password, user.password);
+    if (!passworCorrecto) {
+      res.status(400).send('Password Incorrecto');
+    } else {
+      res.status(200).json({token: createToken(user)})
+    }
+    /* if(user.password === password){
 
       res.status(200).json({token: createToken(user)})
     }else{
       res.status(400).send("la contraseña no es valida");
-    }
+    } */
   }
 });
-
 
 export default authRouter;
