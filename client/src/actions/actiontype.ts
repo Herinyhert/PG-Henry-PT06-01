ce20fb6519f861c0c2cafbe95a5203a212565221
alejandrobalonga <alejandrobalonga@gmail.com>
2022-09-10 11:48:55 -0300

    update actiontype.ts

--- client/src/actions/actiontype.ts
+++ client/src/actions/actiontype.ts
@@ -1,7 +1,7 @@
-export const SET_ERROR = 'SET_ERROR';
+export const SET_ERROR = "SET_ERROR";
 
-export const GET_TOTALARTICULOS = 'GET_TOTALARTICULOS';
-export const GET_DETAIL_PRODUCT = 'GET_DETAIL_PRODUCT';
+export const GET_TOTALARTICULOS = "GET_TOTALARTICULOS";
+export const GET_DETAIL_PRODUCT = "GET_DETAIL_PRODUCT";
 
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const GET_ARTICULOS = "GET_ARTICULOS";
@@ -10,25 +10,15 @@ export const POST_SIGNIN = "POST_SIGNIN";
 export const CLEAR_STATE = "CLEAR_STATE";
 export const SET_DASHBOARDMENU = "SET_DASHBOARDMENU";
 
-export const GET_GOOGLE = "GET_GOOGLE"
-
-
+export const GET_GOOGLE = "GET_GOOGLE";
 
 export const GET_USERS = "GET_USERS";
 export const GET_TOTALUSERS = "GET_TOTALUSERS";
 export const DELETE_USER = "DELETE_USER";
 
-
-export const GET_CATEGORIES = 'GET_CATEGORIES';
+export const GET_CATEGORIES = "GET_CATEGORIES";
 export const DELETE_CATEGORY = "DELETE_CATEGORY";
-
 
 export const DELETE_ORDERS = "DELETE_ORDERS";
 export const GET_ORDERS = "GET_ORDERS";
 export const GET_TOTALORDERS = "GET_TOTALORDERS";
-
-
-
-
-
-
