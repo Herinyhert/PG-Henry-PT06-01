794d1592f427bea89267111bce3c9f3cb7812d54
alejandrobalonga <alejandrobalonga@gmail.com>
2022-09-10 12:00:01 -0300

    update CreateCategory.tsx

--- client/src/components/Dashboard/Dialogs/CreateCategory.tsx
+++ client/src/components/Dashboard/Dialogs/CreateCategory.tsx
@@ -5,26 +5,52 @@ import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
+import { Category, postCategory } from "../../../actions";
+import { useDispatch, useSelector } from "react-redux";
+import { ReduxState } from "../../../reducer";
 
-export default function FormDialog() {
+export interface CardCategoryProps {
+  category: Category;
+}
+
+
+export default function FormDialog({ category }: CardCategoryProps) {
+  const dispatch = useDispatch<any>();
   const [open, setOpen] = React.useState(false);
+  const [input, setInput] = useState({
+    id: category.id,
+    name: category.name,
+  });
+
+  const token = useSelector((state: ReduxState) => state.token);
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-  const handleSubmit = () => {
+  function handleSubmit(e) {
+    console.log(input);
     setOpen(false);
+    e.preventDefault();
+    dispatch(postCategory(token, input));
   };
 
   const handleClose = () => {
     setOpen(false);
   };
+  
+
+  function handlechange(e) {    
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+}
 
   return (
     <div>
       <Button variant="outlined" fullWidth onClick={handleClickOpen}>
-        Create
+        {category.id ? "Update" : "Create"}
       </Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>New Category</DialogTitle>
@@ -34,9 +60,12 @@ export default function FormDialog() {
             margin="dense"
             id="category"
             label="Category"
+            name="name"
             type="text"
             fullWidth
             variant="standard"
+            value={input.name}
+            onChange={(e) => handlechange(e)}
           />
         </DialogContent>
         <DialogActions>
